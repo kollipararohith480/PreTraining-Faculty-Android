9bb1df8b697b77f5f61ee739c6d6650a1921a36e
kollipararohith480 <kollipararohith480@gmail.com>
2016-08-30 11:48:23 +0530

    sxxa

---
+++ sums.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int Sum(int *input, int n);
+ 
+int main()
+{
+    int input[100], count, i;
+     
+    printf("Enter number of elements in Array: ");
+    scanf("%d", &i);
+    printf("Enter %d numbers \n ", i);
+    for(count = 0; count < i; count++)
+    {
+        scanf("%d", &input[count]);
+    }
+     
+    printf("Sum of all numbers are : %d \n",
+    Sum(input,i - 1));
+    return 0;
+}
+
+int Sum(int *input, int n)
+{
+     if(NULL == input || n < 0)
+        return 0;
+     return input[n] + Sum(input, n -1);
+}
